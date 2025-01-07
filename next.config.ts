e6114087139431628c3c4f9fb7f6ff71621e9e09
Kirill171 <kiria12312@mail.ru>
2025-01-07 08:25:37 +0300

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/next-js-landing-page',
+  assetPrefix: '/next-js-landing-page/',
+  trailingSlash: true,
 };
 
 export default nextConfig;
--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -3,23 +3,14 @@
 import {
   motion,
   useAnimate,
-  AnimationPlaybackControls,
 } from 'framer-motion';
-import {
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import { useEffect } from 'react';
 
 export default function CallToAction() {
-  const [isHovered, setIsHovered] =
-    useState(false);
-  const animation =
-    useRef<AnimationPlaybackControls>(null);
   const [scope, animate] = useAnimate();
 
   useEffect(() => {
-    animation.current = animate(
+    animate(
       scope.current,
       { x: '-50%' },
       {
@@ -30,23 +21,12 @@ export default function CallToAction() {
     );
   }, []);
 
-  useEffect(() => {
-    if (animation.current) {
-      if (isHovered) {
-        animation.current.speed = 0.5;
-      } else {
-        animation.current.speed = 1;
-      }
-    }
-  }, [isHovered]);
   return (
     <section className="py-24">
       <div className="overflow-x-clip p-4 flex">
         <motion.div
           ref={scope}
           className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium group cursor-pointer"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
         >
           {Array.from({ length: 10 }).map(
             (_, i) => (
